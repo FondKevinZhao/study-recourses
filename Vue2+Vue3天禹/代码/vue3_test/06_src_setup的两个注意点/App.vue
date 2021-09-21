27e873cdc699c783b3ad618c0a07cda77bd91fdf
FondKevinZhao <443403633@qq.com>
2021-09-21 12:01:21 +0800

    vue2+vue3

---
+++ Vue2+Vue3天禹/代码/vue3_test/06_src_setup的两个注意点/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<Demo @hello="showHelloMsg" msg="你好啊" school="尚硅谷">
+		<template v-slot:qwe>
+			<span>尚硅谷</span>
+		</template>
+		<template v-slot:asd>
+			<span>尚硅谷</span>
+		</template>
+	</Demo>
+</template>
+
+<script>
+	import Demo from './components/Demo'
+	export default {
+		name: 'App',
+		components:{Demo},
+		setup(){
+			function showHelloMsg(value){
+				alert(`你好啊，你触发了hello事件，我收到的参数是:${value}！`)
+			}
+			return {
+				showHelloMsg
+			}
+		}
+	}
+</script>
+
---
+++ Vue2+Vue3天禹/代码/vue_test/06_src_scoped样式/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="demo">
+		<h2 class="title">学校名称：{{name}}</h2>
+		<h2>学校地址：{{address}}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'School',
+		data() {
+			return {
+				name:'尚硅谷atguigu',
+				address:'北京',
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.demo{
+		background-color: skyblue;
+	}
+</style>
